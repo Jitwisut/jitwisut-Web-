e36d6e33357725fdbae829c97e45ef8cdb59bf03
Jitwisut <tanzajitwisut@gmail.com>
2024-05-07 14:18:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const but=document.querySelector(".button")
+but.addEventListener('click',()=>{
+    var link="https://www.udemy.com/course/javascript-building-20-projects/learn/lecture/32501370?start=250#overview"
+   window.open(link)
+})
+
